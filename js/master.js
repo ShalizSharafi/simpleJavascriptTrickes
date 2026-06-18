edf40546c6f6032cf9f80b2f34f2b15a80d4a1d4
shalizsharafi <shalizoputh@gmail.com>
2026-06-18 03:30:47 -0700

    5th commit

--- js/master.js
+++ js/master.js
@@ -6,6 +6,7 @@ const header = document.querySelector('header')
 const closePopUp = document.querySelector('.closePopUp')
 const popUp = document.querySelector('.popUp')
 const popUpWall = document.querySelector('.popUpWall')
+const card = document.querySelectorAll('.card')
 
 themeSwitch.addEventListener('click',()=>{
        let temp = themeSwitch.getAttribute('data-status')
@@ -42,3 +43,18 @@ popUpWall.addEventListener('click',()=>{
 })
 
 ///////////
+
+card.forEach((val)=>{
+       val.classList.add(val.getAttribute('data-class'))
+       val.parentElement.setAttribute('data-top',val.offsetTop)
+})
+
+window.addEventListener('scroll',()=>{
+       let scrollT = window.scrollY
+       console.log(scrollT)
+       card.forEach((val)=>{
+              if(scrollT > (val.parentElement.getAttribute('data-top'))/10){
+              val.classList.remove(val.getAttribute('data-class'))
+       }
+       })
+})
